d8702ff9429c4be7960006e307ca0a43985dfc4e
Mehmood Ahmed <devmehmood.ahmed@gmail.com>
2023-05-05 00:23:28 +0500

    displaying product listings and details

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+products = [
+  {
+    name: 'Product 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    price: 9.99,
+    image: 'product1.jpg'
+  },
+  {
+    name: 'Product 2',
+    description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    price: 19.99,
+    image: 'product1.jpg'
+  },
+  {
+    name: 'Product 3',
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+    price: 29.99,
+    image: 'product1.jpg'
+  },
+  {
+    name: 'Product 4',
+    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse.',
+    price: 39.99,
+    image: 'product1.jpg'
+  },
+  {
+    name: 'Product 5',
+    description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+    price: 49.99,
+    image: 'product1.jpg'
+  }
+]
+
+products.each do |product|
+  Product.create(product)
+end
